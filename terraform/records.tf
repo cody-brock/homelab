7f1af632519aeed2f748a536f1fbc562c09c9cfa
Cody Brock <51219646+cody-brock@users.noreply.github.com>
2026-07-25 21:58:07 -0700

    Set up remote state in r2 for terraform (#1)
    * terraform fmt
    
    * Add remote backend via R2 for terraform. Bump tf required version to >=1.10 for use_lockfile

--- terraform/records.tf
+++ terraform/records.tf
@@ -3,55 +3,55 @@
 
 # __generated__ by Terraform from "6bd1f01ce4f83b1bfd29abf400ed5a28/bc5679745b3e21446c8c5cd89f8c1cef"
 resource "cloudflare_dns_record" "apex_a_109" {
-  content         = "185.199.109.153"
-  name            = "codybrock.dev"
-  proxied         = false
-  ttl             = 1
-  type            = "A"
-  zone_id         = var.zone_id
+  content = "185.199.109.153"
+  name    = "codybrock.dev"
+  proxied = false
+  ttl     = 1
+  type    = "A"
+  zone_id = var.zone_id
 }
 
 # __generated__ by Terraform from "6bd1f01ce4f83b1bfd29abf400ed5a28/942b46d8937bdaa14dc7d058dd66f446"
 resource "cloudflare_dns_record" "apex_a_110" {
-  content         = "185.199.110.153"
-  name            = "codybrock.dev"
-  proxied         = false
-  ttl             = 1
-  type            = "A"
-  zone_id         = var.zone_id
+  content = "185.199.110.153"
+  name    = "codybrock.dev"
+  proxied = false
+  ttl     = 1
+  type    = "A"
+  zone_id = var.zone_id
 }
 
 # __generated__ by Terraform from "6bd1f01ce4f83b1bfd29abf400ed5a28/95161deaea448f1f33f6a70d201b4ebb"
 resource "cloudflare_dns_record" "apex_a_108" {
-  content         = "185.199.108.153"
-  name            = "codybrock.dev"
-  proxied         = false
-  ttl             = 1
-  type            = "A"
-  zone_id         = var.zone_id
+  content = "185.199.108.153"
+  name    = "codybrock.dev"
+  proxied = false
+  ttl     = 1
+  type    = "A"
+  zone_id = var.zone_id
 }
 
 # __generated__ by Terraform from "6bd1f01ce4f83b1bfd29abf400ed5a28/0ef660ea1d2282191fef2a6c7558defc"
 resource "cloudflare_dns_record" "www_cname_github" {
-  content         = "cody-brock.github.io"
-  name            = "www.codybrock.dev"
-  proxied         = false
+  content = "cody-brock.github.io"
+  name    = "www.codybrock.dev"
+  proxied = false
   settings = {
     flatten_cname = false
     ipv4_only     = false
     ipv6_only     = false
   }
-  ttl             = 1  # in Cloudflare, 1 means auto
-  type            = "CNAME"
-  zone_id         = var.zone_id
+  ttl     = 1 # in Cloudflare, 1 means auto
+  type    = "CNAME"
+  zone_id = var.zone_id
 }
 
 # __generated__ by Terraform from "6bd1f01ce4f83b1bfd29abf400ed5a28/9ea37079914a22379aa59360c07042b8"
 resource "cloudflare_dns_record" "apex_a_111" {
-  content         = "185.199.111.153"
-  name            = "codybrock.dev"
-  proxied         = false
-  ttl             = 1
-  type            = "A"
-  zone_id         = var.zone_id
+  content = "185.199.111.153"
+  name    = "codybrock.dev"
+  proxied = false
+  ttl     = 1
+  type    = "A"
+  zone_id = var.zone_id
 }
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,5 +1,19 @@
 terraform {
-  required_version = ">= 1.5"
+  required_version = ">= 1.10"
+
+  backend "s3" {
+    bucket                      = "codybrock-tf-state"
+    key                         = "homelab/terraform.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+    use_lockfile                = true
+    endpoints                   = { s3 = "https://cfa22f2d5f7788986f1c61c6aa147e85.r2.cloudflarestorage.com" }
+  }
 
   required_providers {
     cloudflare = {
